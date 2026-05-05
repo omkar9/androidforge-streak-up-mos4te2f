334945bd79bf8df003803301ad6fd92c3aa27e3b
omkar9 <omkar946@gmail.com>
2026-05-05 10:03:10 +0530

    feat: add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // These plugins are typically defined in settings.gradle.kts and applied in individual module build.gradle.kts files
+    // For a modern multi-module project, this file is often minimal.
+    // Refer to settings.gradle.kts for plugin management.
+}
